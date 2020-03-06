70c2fbbf83f413d80d37b3fe9695753a61d89274
OrbmasWin <sambrostudent@gmail.com>
2020-03-06 20:44:37 +0800

    time deducted from timer on incorrect answer

--- quiz.js
+++ quiz.js
@@ -3,10 +3,13 @@ var timeEl = document.querySelector(".timer-out");
 
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
+const finishButton = document.getElementById("finish-btn");
 const questionContainerEl = document.getElementById("question-container");
-const questionEl = document.getElementById("question")
+const questionEl = document.getElementById("question");
 const answerButtons = document.getElementById("answer-btns");
+const ansResult = document.getElementById("result");
 
+var timerInterval;
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -14,11 +17,15 @@ startButton.addEventListener("click", function() {
     startGame()
     setTimer()
 });
-
+nextButton.addEventListener('click', function() {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 //timer elements
 var secondsLeft = 100;
 
 //timer function
+
 function setTimer() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -46,6 +53,7 @@ function startGame(){
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+    ansResult.classList.add('hide')
 }
 
 
@@ -72,10 +80,39 @@ function resetState() {
 
 
 
-function selectAnswer(e) {
-
+function selectAnswer(e, timerInterval) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    //create an array from answer buttons
+    Array.from(answerButtons.children).forEach(function(button) {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    } else {
+    finishButton.classList.remove('hide')
+    clearInterval(timerInterval);
+    }
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add("correct")
+        ansResult.innerText = ("Correct!")
+        ansResult.classList.remove("hide")
+    } else {
+        element.classList.add("wrong")
+        ansResult.innerText = ("Incorrect!")
+        secondsLeft = secondsLeft - 2.5; 
+    }
+}
+
+function clearStatusClass(element) {
+        element.classList.remove("correct")
+        element.classList.remove("wrong")
+}
 
 const questions = [
     {
@@ -87,7 +124,28 @@ const questions = [
             {text: 'Wrong answer', correct: false},
 
         ]
+    },
+    {
+        question: 'What is something that is a question?',
+        answers: [
+            {text: 'correct answer', correct: true},
+            {text: 'Wrong answer', correct: false},
+            {text: 'Wrong answer', correct: false},
+            {text: 'Wrong answer', correct: false},
+
+        ]
+    },
+    {
+        question: 'What is something that is a question?',
+        answers: [
+            {text: 'correct answer', correct: true},
+            {text: 'Wrong answer', correct: false},
+            {text: 'Wrong answer', correct: false},
+            {text: 'Wrong answer', correct: false},
+
+        ]
     }
+    
 ]
 
 // remove 10 seconds on false test
